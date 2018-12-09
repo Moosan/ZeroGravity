7cf366e1ed4389b7198535f1e01e3638168c82f1
Moosan <washer2016@outlook.jp>
2018-12-09 21:31:34 +0900

    FirstCommit

---
+++ Assets/VRSample/Scripts/Extra/Bomb.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using Valve.VR.InteractionSystem;
+
+public class Bomb : VRObjectBase{
+
+    public GameObject Effect;
+    public float BomTime;
+    private bool Picked;
+    private bool LetsBom;
+    
+    public void Update() {
+        if (Picked)
+        {
+            if (device != null)
+            {
+                if (device.GetPressDown(SteamVR_Controller.ButtonMask.Touchpad))
+                {
+                    if (LetsBom)
+                    {
+                        return;
+                    }
+                    Invoke("Dokan", BomTime);
+                    LetsBom = true;
+                }
+            }
+            else if (Input.GetKey(KeyCode.Q))
+            {
+                if (LetsBom)
+                {
+                    return;
+                }
+                Invoke("Dokan", BomTime);
+                LetsBom = true;
+            }
+        }
+    }
+    public void BombMoti()
+    {
+        Debug.Log("爆弾を持った！！！");
+        Picked = true;
+    }
+    public void BombHanasi()
+    {
+        Debug.Log("爆弾を離した！！！");
+        Picked = false;
+    }
+
+    private void Dokan() {
+        GameObject obj;
+        if (Picked) {
+            obj=(GameObject)Instantiate(Effect,transform.parent.position,new Quaternion());
+            obj.GetComponent<Explosion>().device =device;
+            Hand.DetachObject(gameObject);
+        }
+        else
+        {
+            obj=(GameObject)Instantiate(Effect,transform.position,new Quaternion());
+        }
+        
+        Destroy(obj,2.1f);
+        Destroy(gameObject.GetComponent<MeshRenderer>(),1.0f);
+        DestroyImmediate(GetComponent<Throwable>());
+        DestroyImmediate(rigidBody);
+        Destroy(gameObject,8);
+        Destroy(this,10);
+    }
+}
---
+++ Assets/VRSample/SteamVR/Scripts/SteamVR_GameView.cs
@@ -0,0 +1,19 @@
+﻿//======= Copyright (c) Valve Corporation, All rights reserved. ===============
+//
+// Purpose: Handles rendering to the game view window
+//
+//=============================================================================
+
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SteamVR_GameView : MonoBehaviour
+{
+	void Awake()
+	{
+		Debug.Log("SteamVR_GameView is deprecated in Unity 5.4 - REMOVING");
+        Debug.Log("SteamVR_GameViewというクラスは、このバージョンのUnityでは非推奨なため、削除されました。");
+		DestroyImmediate(this);
+	}
+}
+
