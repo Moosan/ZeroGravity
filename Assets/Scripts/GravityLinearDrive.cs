968628737a1d36addd1c89961a818ae3b4cc6c01
Moosan <washer2016@outlook.jp>
2018-12-17 12:51:36 +0900

    gravityhandle

--- Assets/Scripts/GravityLinearDrive.cs
+++ Assets/Scripts/GravityLinearDrive.cs
@@ -13,7 +13,16 @@ public class GravityLinearDrive : LinearDrive {
     protected override void HandHoverUpdate(Hand hand)
     {
         base.HandHoverUpdate(hand);
-        var value = linearMapping.value * valueMax;
+        ChangeGravity(linearMapping.value * valueMax);
+    }
+    public void ChangeValue(float value)
+    {
+        linearMapping.value = value / valueMax;
+        transform.position = Vector3.Lerp(startPosition.position, endPosition.position, linearMapping.value);
+        ChangeGravity(value);
+    }
+    private void ChangeGravity(float value)
+    {
         slider.value = value;
         Physics.gravity = new Vector3(0, value * 2.0f * -9.81f, 0);
     }
