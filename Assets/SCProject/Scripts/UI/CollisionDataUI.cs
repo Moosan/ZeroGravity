c173e0db3490faabfa1c1b296752e047d9f55615
Moosan <washer2016@outlook.jp>
2019-02-28 07:04:39 +0900

    UIいろいろ

---
+++ Assets/SCProject/Scripts/UI/CollisionDataUI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class CollisionDataUI : MonoBehaviour {
+    public Transform StartTransform;
+    public Transform EndTransform;
+    public Text TimeText;
+    public Text DistanceText;
+    private LineRenderer Line;
+    private void Awake()
+    {
+        Line = GetComponent<LineRenderer>();
+        Line.SetPositions(new Vector3[] { StartTransform.position, EndTransform.position });
+    }
+    public void SetParameter(float time,float distance)
+    {
+        TimeText.text = "滞空時間: " + time.ToString("f2") + " s";
+        DistanceText.text = "飛距離　: " + distance.ToString("f2") + " m";
+    }
+}
---
+++ Assets/SCProject/Scripts/UI/ThrowDataUI.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class ThrowDataUI : MonoBehaviour {
+    public Transform StartTransform;
+    public Transform EndTransform;
+    public Text MassText;
+    public Text HeightText;
+    public Text VelocityText;
+    private LineRenderer Line;
+    private void Awake()
+    {
+        Line = GetComponent<LineRenderer>();
+        Line.SetPositions(new Vector3[] { StartTransform.position, EndTransform.position });
+    }
+    public void SetParameter(float mass,float height,float velocity)
+    {
+        MassText.text = "質量: " + mass.ToString("f2") + " kg";
+        HeightText.text = "高さ: " + height.ToString("f2") + " m";
+        VelocityText.text = "速さ: " + velocity.ToString("f2") + " m/s";
+    }
+}
