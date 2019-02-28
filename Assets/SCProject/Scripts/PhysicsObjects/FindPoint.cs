ab029195bbe5eb76e576e1532eadceea9ee3df8f
HnakadouriN <hasebenaoki0908@docomo.ne.jp>
2019-02-28 16:44:58 +0900

    コルーーチン!!

--- Assets/SCProject/Scripts/PhysicsObjects/FindPoint.cs
+++ Assets/SCProject/Scripts/PhysicsObjects/FindPoint.cs
@@ -9,7 +9,6 @@ public class FindPoint : VRObjectBase
     private float TotalTime;
     public GameObject Point;
     private bool create;
-    private bool count;
     private float kyori;
     private Vector3 startPosi;
     // Use this for initialization
@@ -18,38 +17,41 @@ public class FindPoint : VRObjectBase
         rb = GetComponent<Rigidbody>();
         obj2 = GameObject.Find("Cube");
         create = true;
-        count = false;
         //rb.AddForce(Vector3.right*5f);
         //OnThrow();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (count)
-        {
-            TotalTime += Time.deltaTime;
-        }
-    }
+   
     private void OnCollisionEnter(Collision other)
     {
         if (create)
         {
-           var obj =  Instantiate(Point, transform.position, Quaternion.identity);
-            Debug.Log(TotalTime);
-            Vector3 endPosi = this.transform.position;
-            Debug.Log(endPosi);
-            kyori = Mathf.Sqrt((endPosi.x - startPosi.x) * (endPosi.x - startPosi.x)+(endPosi.z - startPosi.z)* (endPosi.z - startPosi.z));
-            obj.GetComponent<CollisionDataUI>().SetParameter(TotalTime, kyori);
+            StartCoroutine(seisei());
             create = false;
-            count = false;
+            StopCoroutine(keisoku());
             TotalTime = 0f;
+
         }
     }
     public void OnThrow(){
         startPosi = this.transform.position;
         Debug.Log(startPosi);
-        count = true;
+        StartCoroutine(keisoku());
+    }
+    private IEnumerator keisoku(){
+        while(true){
+            TotalTime += Time.deltaTime;
+            yield return null;
+        }
+    }
+    private IEnumerator seisei(){
+        var obj = Instantiate(Point, transform.position, Quaternion.identity);
+        Debug.Log(TotalTime);
+        Vector3 endPosi = this.transform.position;
+        Debug.Log(endPosi);
+        kyori = Mathf.Sqrt((endPosi.x - startPosi.x) * (endPosi.x - startPosi.x) + (endPosi.z - startPosi.z) * (endPosi.z - startPosi.z));
+        obj.GetComponent<CollisionDataUI>().SetParameter(TotalTime, kyori);
+        yield return null;
     }
 }
 
