d25e02e5fc00f310dc8187637361276f68c978fd
Moosan <washer2016@outlook.jp>
2019-03-10 16:35:13 +0900

    AddBall

---
+++ Assets/SCProject/Scripts/PhysicsObjects/Instantiator.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiator : MonoBehaviour {
+    private Vector3 StartPos;
+    [SerializeField]
+    private GameObject Prefab;
+    [SerializeField]
+    private float LifeTime;
+    private float TimeCount;
+    private void Awake()
+    {
+        StartPos = transform.position;
+        TimeCount = 0;
+    }
+
+    public void Instant()
+    {
+        var obj = (GameObject)Instantiate(Prefab, StartPos, new Quaternion());
+        obj.transform.parent = this.transform;
+
+    }
+
+    private void Update()
+    {
+        TimeCount += Time.deltaTime;
+        if(TimeCount >= LifeTime)
+        {
+            TimeCount = 0;
+            if (!ExistChild())
+            {
+                Instant();
+            }
+        }
+    }
+
+    private bool ExistChild()
+    {
+        return transform.childCount >= 1;
+    }
+}
