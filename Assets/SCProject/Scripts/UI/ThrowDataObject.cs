6e01ea6981e18eaf72ce0da9678843f04675f8f5
Moosan <washer2016@outlook.jp>
2019-03-19 22:16:01 +0900

    MergeMire

--- Assets/SCProject/Scripts/UI/ThrowDataObject.cs
+++ Assets/SCProject/Scripts/UI/ThrowDataObject.cs
@@ -11,24 +11,35 @@ public class ThrowDataObject :MonoBehaviour{
     private Rigidbody Rigidbody;
     private GameObject throwdata;
     private Vector3 ThrowPos;
+    [SerializeField]
+    private float DelayTime;
 
     [SerializeField]
     private bool IsThrow;
 
-	// Use this for initialization
-	void Awake () {
-        Rigidbody = this.GetComponent<Rigidbody>();
-        
-    }
     private void Start()
     {
+        Rigidbody = this.GetComponent<Rigidbody>();
         if (IsThrow)
         {
             OnThrow();
         }
     }
+    public IEnumerator loop()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(DelayTime);
+            MakeUI();
+        }
+    }
+    public void OnThrow()
+    {
+        MakeUI();
+        StartCoroutine("loop");
+    }
 
-    public void OnThrow(){
+    private void MakeUI(){
         ThrowPos = this.transform.position;
         throwdata = Instantiate(ThrowDataUI, ThrowPos,Quaternion.identity);
         height = this.transform.position.y;
@@ -37,5 +48,12 @@ public class ThrowDataObject :MonoBehaviour{
         throwdata.GetComponent<ThrowDataUI>().SetParameter(mass, height, velocity);
         throwdata.SetActive(true);
         IsThrow = false;
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Floor")
+        {
+            StopCoroutine("loop");
+        }
     }
 }
