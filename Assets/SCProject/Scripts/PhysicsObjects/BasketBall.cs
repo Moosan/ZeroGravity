437bb7403b177406fe47419409d57f881312677e
OSK-Master <os_ken@ed.tus.ac.jp>
2019-03-23 23:34:09 +0900

    Kansei

--- Assets/SCProject/Scripts/PhysicsObjects/BasketBall.cs
+++ Assets/SCProject/Scripts/PhysicsObjects/BasketBall.cs
@@ -3,5 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BasketBall : VRObjectBase {
-    
+    public GameObject Bound;
+    void OnCollisionEnter(Collision collision)
+    {
+        var contacts = collision.contacts;
+        var obj = Instantiate(Bound, contacts[0].point, new Quaternion());
+        obj.GetComponent<AudioSource>().volume = collision.relativeVelocity.magnitude/10;
+        Destroy(obj, 0.7f);
+    }
+
 }
